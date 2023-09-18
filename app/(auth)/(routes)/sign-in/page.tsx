a3a277592d9706510ad68939b05b5b33d0f36df2
Piotr Śmietanka <piotr.smietanka@widelab.co>
2023-09-18 20:10:23 +0200

    chore: init dashboard & auth layouts

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,5 @@
+const SignIn = () => {
+  return <div>This is an unprotected page</div>;
+};
+
+export default SignIn;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -3,8 +3,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div>
-      <Button variant="link">Click me</Button>
-      <p className="text-red-500 font-medium">Hello</p>;
+      <p className="text-red-500 font-medium">This is a protected page</p>
     </div>
   );
 }
